c6583157421bae70bb22bc438ec027839c35d5a6
AlexeyManankov <virtbrain@gmail.com>
2023-08-26 20:53:00 +0500

    done

---
+++ MVC/Model/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MVC
+//
+//  Created by Alexey Manankov on 26.08.2023.
+//
+
+
+//MARK: - Model Person
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
--- MVC/ViewController.swift
+++ MVC/ViewController.swift
@@ -7,16 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//MARK: - View + Controller
+class GreetingViewController: UIViewController {
+    
+    let showGretteingButton = UIButton()
+    let greetingLabel = UILabel()
+    
+    let person = Person(firstName: "David", lastName: "Blaine")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         view.backgroundColor = .white
         title = "Model View Controller"
+        setupView()
         
     }
-
-
+    
+    private func setupView() {
+        [greetingLabel, showGretteingButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        greetingLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        showGretteingButton.backgroundColor = .green
+        showGretteingButton.setTitle("Show greeting", for: .normal)
+        showGretteingButton.setTitleColor(.black, for: .normal)
+        showGretteingButton.setTitleColor(UIColor.init(white: 1, alpha: 0.3), for: .highlighted)
+        showGretteingButton.layer.cornerRadius = 10
+        
+        NSLayoutConstraint.activate([
+            greetingLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            greetingLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            
+            showGretteingButton.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 40),
+            showGretteingButton.widthAnchor.constraint(equalToConstant: 200),
+            showGretteingButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            showGretteingButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        showGretteingButton.addTarget(self, action: #selector(didTapGreetingButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapGreetingButton() {
+        let greeting = "Hello" + " " + self.person.firstName + " " + self.person.lastName
+        self.greetingLabel.text = greeting
+    }
 }
-
